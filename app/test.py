55bee68bcc4197ae42497648fc3f40cc086b1860
mksmp <panin.ma2013u@mail.ru>
2022-12-01 17:59:00 +0300

    add tests

--- app/test.py
+++ app/test.py
@@ -7,12 +7,12 @@ import requests
 class TestAPI(unittest.TestCase):
 
     def test1(self):
-        resp = requests.get("http://172.128.0.3:5000/")
+        resp = requests.get("http://127.0.0.1:5000/")
         self.assertEqual(resp.status_code, 200)
         print("test1: OK")
         
     def test2(self):
-        resp = requests.get("http://172.128.0.3:5000/accident/getAll")
+        resp = requests.get("http://127.0.0.1:5000/accident/getAll")
         self.assertEqual(resp.status_code, 200)
         print("test2: OK")
         
@@ -24,12 +24,12 @@ class TestAPI(unittest.TestCase):
         'end': "2022-11-02"
         }
 
-        resp = requests.get("http://172.128.0.3:5000/accident/getBetween", data=json.dumps(data), headers=headers)
+        resp = requests.get("http://127.0.0.1:5000/accident/getBetween", data=json.dumps(data), headers=headers)
         self.assertEqual(resp.status_code, 200)
         print("test3: OK")
 
     def test4(self):
-        resp = requests.post("http://172.128.0.3:5000/accident/create")
+        resp = requests.post("http://127.0.0.1:5000/accident/create")
         self.assertEqual(resp.status_code, 200)
         print("test4: OK")
 
