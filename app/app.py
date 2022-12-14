82194a933a6f3a77de6c041eb6208a2c8f7b5719
mksmp <panin.ma2013u@mail.ru>
2022-12-14 23:44:46 +0300

    add files

--- app/app.py
+++ app/app.py
@@ -1,22 +1,18 @@
-import pymysql.cursors
-import requests
-from config import MYSQL_DATABASE, MYSQL_HOST, MYSQL_PASSWORD, MYSQL_USER
 from flask import Flask, jsonify, request
+from flask_migrate import Migrate
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 application = app
 
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://admin:admin@pr_incident_db/project_incident_db'
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://std_1760_pd1:std_1760_pd1@std-mysql.ist.mospolytech.ru/std_1760_pd1'
+
 app.config.from_pyfile('config.py')
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
-# mysql = pymysql(app)
-
-connection = pymysql.connect(host=MYSQL_HOST,
-                            user=MYSQL_USER,
-                            password=MYSQL_PASSWORD,
-                            database=MYSQL_DATABASE,
-                            charset='utf8mb4',
-                            cursorclass=pymysql.cursors.DictCursor)
-
+from models import Accidents
 
 
 @app.route('/', methods=['GET'])
@@ -32,182 +28,18 @@ def get_my_ip():
 
 @app.route('/accident/getAll', methods=['GET'])
 def getAllAccidents():
-    with connection.cursor() as cursor:
-        sql = 'SELECT * FROM `Members` JOIN `accidents_members` on `Members`.`id` = `accidents_members`.`members_id`;'
-        cursor.execute(sql)
-        members = cursor.fetchall()
-
-    print(members)
-    print()
-    print()
-    print()
-    with connection.cursor() as cursor:
-        sql = 'SELECT accidents.id, accidents.name, accidents.description, types.type, accidents.date FROM accidents INNER JOIN types on accidents.type_id = types.id;'
-        cursor.execute(sql)
-        res = cursor.fetchall()
-
-    print(res)
-    if len(res) == 0:
-        return jsonify({'error': "Данных нет"})
-    print(res)
+    s = Accidents.query.all()
+    print(s)
     l = list()
-    for i in range(len(res)):
+    for i in s:
         data = {
-            'id': res[i]["id"],
-            'Название': res[i]["name"],
-            'Описание': res[i]["description"],
-            'Тип': res[i]["type"],
-            'Дата': res[i]["date"],
-            'Участники': list()
-        }
-        print(res[i]["id"])
-        print()
-        for j in members:
-            print(j["accidents_id"])
-            if res[i]["id"] == j["accidents_id"]:
-               full_name = j['first_name'] + " " + j['initials'] 
-               data['Участники'].append(full_name)
-        l.append(data)
-        print()
-
-    return jsonify(l)
-
-@app.route('/accident/getById', methods=['GET'])
-def getAccidentsById():
-    data = {
-        'id': request.json['id'],
-    }
-    with connection.cursor() as cursor:
-        sql = 'SELECT accidents.name, accidents.description, types.type, accidents.date FROM accidents INNER JOIN types on accidents.type_id = types.id WHERE accidents.id = %s;'
-        cursor.execute(sql, (data['id']))
-        res = cursor.fetchall()
-    if len(res) == 0:
-        return jsonify({'error': "Данных нет"})
-    data = {
-        'Название': res[0]["name"],
-        'Описание': res[0]["description"],
-        'Тип': res[0]["type"],
-        'Дата': res[0]["date"],
-    }
-
-    return jsonify(data)
-
-
-@app.route('/accident/getBetween', methods=['GET'])
-def getBetween():
-    data = {
-        'start': request.json['start'],
-        'end': request.json['end'],
-    }
-
-    with connection.cursor() as cursor:
-        sql = "SELECT accidents.name, accidents.description, types.type, accidents.date FROM accidents INNER JOIN types on accidents.type_id = types.id WHERE accidents.date BETWEEN %s and %s;"
-        cursor.execute(sql, (data['start'], data['end']))
-        res = cursor.fetchall()
-    if len(res) == 0:
-        return jsonify({'error': "Данных нет"})
-
-    l = list()
-    for i in range(len(res)):
-        data = {
-            'Название': res[i]["name"],
-            'Описание': res[i]["description"],
-            'Тип': res[i]["type"],
-            'Дата': res[i]["date"],
+            'id': i.id,
+            'Название': i.name,
+            'Описание': i.description,
+            'Тип': i.type_id,
+            'Дата': i.date,
         }
         l.append(data)
 
+
     return jsonify(l)
-
-
-@app.route('/accident/create', methods=['POST'])
-def create():
-    data = {
-        'name': request.json['name'],
-        'description': request.json['description'],
-        'date': request.json['date'],
-        'type': request.json['type'],
-        'members': request.json['members'],
-    }
-    with connection.cursor() as cursor:
-        sql = 'SELECT * FROM Members'
-        cursor.execute(sql)
-        members = cursor.fetchall()
-    
-    # добавляю тех members, которых нет в БД 
-    d_members = dict()
-    for i in members:
-        full_name = i['first_name'] + " " + i['initials']
-        for j in data['members']:
-            if full_name == j:
-                d_members[j] = i['id']
-    for j in data['members']:
-        if d_members.get(j) == None:
-            s = j.split(" ")
-            if len(s) != 2:
-                err = "неверный формат участника '%s'" % (j)
-                return jsonify({'ошибка': err })
-            with connection.cursor() as cursor:
-                sql = 'INSERT INTO `Members` (`id`, `first_name`, `initials`) VALUES (NULL, %s, %s);'
-                cursor.execute(sql, (s[0], s[1]))
-                connection.commit()
-
-    # получаю все members (могут изменится после добавления)
-    members_list = list()
-
-    with connection.cursor() as cursor:
-        sql = 'SELECT * FROM Members'
-        cursor.execute(sql)
-        members = cursor.fetchall()
-
-    for i in members:
-        full_name = i['first_name'] + " " + i['initials']
-        for j in data['members']:
-            if full_name == j:
-                members_list.append(i['id'])
-
-    with connection.cursor() as cursor:
-        sql = 'SELECT * FROM types'
-        cursor.execute(sql)
-        types = cursor.fetchall()
-
-    type_id = 0
-    for i in types:
-        if i['type'] == data['type']:
-            type_id = i['id']
-    if type_id == 0:
-        err = "данного типа происшествия не существует '%s'" % (data['type'])
-        return jsonify({'ошибка': err })
-
-    with connection.cursor() as cursor:
-        sql = 'SELECT MAX(id) FROM Members'
-        cursor.execute(sql)
-        max = cursor.fetchall()
-
-    max_ind = int(max[0]['MAX(id)']) + 1
-
-    with connection.cursor() as cursor:
-                sql = "INSERT INTO `accidents` (`id`, `name`, `description`, `date`, `type_id`) VALUES (%s, %s, %s, %s, %s);"
-                cursor.execute(sql, (max_ind, data['name'], data['description'], data['date'], type_id))
-                connection.commit()
-
-    for i in members_list:
-        with connection.cursor() as cursor:
-            sql = "INSERT INTO `accidents_members` (`accidents_id`, `members_id`) VALUES ('%s', '%s');;"
-            cursor.execute(sql, (max_ind, i))
-            connection.commit()
-
-    return jsonify({'result': 'ок'})
-
-
-@app.route('/accident/update', methods=['POST'])
-def update():
-    with connection.cursor() as cursor:
-        sql = 'UPDATE'
-        cursor.execute(sql)
-        data_msgs = cursor.fetchall()
-    return jsonify({'task': "OK"})
-
-
-# if __name__=='__main__':
-#     app.run()
---
+++ app/models.py
@@ -0,0 +1,36 @@
+from app import db
+
+class Accidents(db.Model):
+    __tablename__ = 'accidents'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), unique=True, nullable=False)
+    description = db.Column(db.String(200), nullable=False)
+    date = db.Column(db.DateTime, nullable=False)
+    type_id = db.Column(db.Integer, db.ForeignKey('types.id'))
+
+    def __repr__(self):
+        return '<Accidents: %r>' % self.name
+
+
+class Types(db.Model):
+    __tablename__ = 'types'
+    id = db.Column(db.Integer, primary_key=True)
+    type = db.Column(db.String(100), nullable=False)
+    
+    def __repr__(self):
+        return '<Types: %r>' % self.name
+
+accidents_members = db.Table(
+    "accidents_members",
+    db.Column("accident_id", db.ForeignKey("accidents.id"), primary_key=True),
+    db.Column("member_id", db.ForeignKey("members.id"), primary_key=True),
+)
+
+class Members(db.Model):
+    __tablename__ = 'members'
+
+    id = db.Column(db.Integer, primary_key=True)
+    member = db.Column(db.String(100), nullable=False, unique=True)
+
+    def __repr__(self):
+        return f'<Members: {self.member}>'
